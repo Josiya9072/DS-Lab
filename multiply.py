92d17867d90df7d74553cddd575b5d0a1eb0a003
Josiya9072 <josiyachacko78@gmail.com>
2025-08-22 14:53:37 +0530

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,8 @@
+arr1 = [1, 2, 3]
+arr2 = [4, 5, 6]
+
+result = []
+for i in range(len(arr1)):
+    result.append(arr1[i] * arr2[i])
+
+print(result)
---
+++ numpy21.py
@@ -0,0 +1,9 @@
+import numpy21 as np
+nums1 = array([[2, 5, 2], [1, 5, 5]])
+nums2 = array([[5, 3, 4], [3, 2, 5]])
+print("array1:")
+print(nums1)
+print("array2:")
+print(nums2)
+print("\n multiply said arrays of same size element-by-element:")
+print(multiply(nums1, nums2))
